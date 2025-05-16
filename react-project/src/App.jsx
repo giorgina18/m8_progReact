e9d334b96e35a04b72b00d83b31adef4f5bed655
giorgina18 <144676972+giorgina18@users.noreply.github.com>
2025-05-16 10:51:37 +0200

    react/ JSX basis KITTYYYYYY

---
+++ react-project/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  // alles wat in return staat word gezien op we web pagina
+  // het is ook sort van html met javascript
+  return (
+
+    <div className='kitty'>
+      <h1 className="h1">dit is een hele schattige foto of a cute kitty :3 </h1>
+      <img className="aboutKitty__img" src="img/kittyyyyyyyyy.jpg" alt="picture of a kittyyyyyyy" />
+      <p className="p">deze kittens zijn pas geboren en zijn op zoek naar een huisje in de buurt van alkmaar.</p>
+    </div>
+  )
+}
+
+export default App
